2ded2ee1ef7b7658e317fb2995ca8effe4fac5ad
Yehor Podporinov <yehor.podporinov@Yehors-MacBook-Pro.local>
2024-06-09 23:06:08 +0300

    added global types

---
+++ src/types.ts
@@ -0,0 +1,36 @@
+import type { BigNumberish, providers } from 'ethers'
+import type { factories } from './contracts'
+
+type ContractFactoryKey = keyof typeof factories
+type ContractFactoryClass<FK extends ContractFactoryKey> =
+  (typeof factories)[FK]
+
+type ContractInstance<FK extends ContractFactoryKey> = ReturnType<
+  ContractFactoryClass<FK>['connect']
+>
+
+type ContractInterface<FK extends ContractFactoryKey> =
+  ReturnType<ContractFactoryClass<FK>['createInterface']>
+
+export type Contract<K extends ContractFactoryKey> = {
+  iface: ContractInterface<K>
+  providerBased: ContractInstance<K>
+  signerBased: ContractInstance<K>
+}
+
+export type Provider = providers.StaticJsonRpcProvider | providers.Web3Provider
+
+export type Wallet = {
+  address: string
+  provider: providers.ExternalProvider | null
+  chainId: number,
+  isConnected: boolean,
+  connect: () => Promise<void>,
+  disconnect: () => Promise<void>,
+  selectChain: (id: number) => Promise<void>
+}
+
+export type WalletBalance = {
+  ethAmount: BigNumberish | null
+  wEthAmount: BigNumberish | null
+}
