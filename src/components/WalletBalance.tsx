d26f665deafb71e06a92ee6e332bf501dc0a393d
Yehor Podporinov <yehor.podporinov@Yehors-MacBook-Pro.local>
2024-06-09 23:25:53 +0300

    added WalletBalance

---
+++ src/components/WalletBalance.tsx
@@ -0,0 +1,44 @@
+import { Web3Context } from '../contexts'
+import { Typography } from '@mui/material'
+import { utils } from 'ethers'
+import { useContext, useMemo } from 'react'
+
+import styled from 'styled-components'
+
+const StyledWrapper = styled.div`{
+  display: grid;
+  grid-gap: 8px;
+}`
+
+export default function WalletBalance() {
+  const { balance } = useContext(Web3Context)
+
+  const indicators = useMemo(() => [
+    {
+      title: 'ETH',
+      value: balance?.ethAmount ? utils.formatEther(balance.ethAmount) : '',
+    },
+    {
+      title: 'WETH',
+      value: balance?.wEthAmount ? utils.formatEther(balance.wEthAmount) : '',
+    },
+  ], [balance])
+
+  return (
+    <StyledWrapper>
+      {indicators.map(indicator =>
+        <Typography
+          variant="h6"
+          component="p"
+          color="text.primary"
+          textAlign="left"
+          overflow="hidden"
+          textOverflow="ellipsis"
+          whiteSpace="nowrap"
+        >
+          {`${indicator.title}: ${indicator.value}`}
+        </Typography>
+      )}
+    </StyledWrapper>
+  )
+}
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as WalletBalance } from './WalletBalance'
+export { default as DepositForm } from './DepositForm'
+export { default as ProcessModal } from './ProgressModal'
+export { default as WithdrawForm } from './WithdrawForm'
